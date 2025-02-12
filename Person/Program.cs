9074fbe25959838f3b2eb61495b35b116a458e79
Mazzinii <luizeduardomazzini@gmail.com>
2025-02-11 23:41:28 -0300

    Added Entity Framework and sqlite

--- Person/Program.cs
+++ Person/Program.cs
@@ -1,5 +1,4 @@
-
-
+using Person.Data;
 using Person.Route;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<PersonContext>();
 
 var app = builder.Build();
 
