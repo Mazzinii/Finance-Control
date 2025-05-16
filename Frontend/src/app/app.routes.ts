83bd1e5344be2b2ce13ac0d5ca4cba5c98df40a6
Mazzinii <luizeduardomazzini@gmail.com>
2025-05-16 12:54:22 -0300

    feat: implement homepage layout

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,6 +2,4 @@ import { Routes } from '@angular/router';
 import { FormCreateComponent } from './components/form-create/form-create.component';
 import { HomeImageComponent } from './components/home-image/home-image.component';
 
-export const routes: Routes = [
-  { path: 'home-login', component: HomeImageComponent },
-];
+export const routes: Routes = [{ path: '', component: FormCreateComponent }];
