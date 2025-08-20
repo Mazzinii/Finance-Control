282e630d22f8e66a25aad518a277b0993a8ce764
Mazzinii <luizeduardomazzini@gmail.com>
2025-08-20 20:44:30 -0300

    feat: Add ngx-echarts to implement graphics

--- Frontend/src/app/components/summary/summary.component.ts
+++ Frontend/src/app/components/summary/summary.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Transation } from '../../models/transation.model';
+import { CommonModule } from '@angular/common';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { BarChart } from 'echarts/charts';
+import { GridComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+import { EChartsCoreOption } from 'echarts/core';
+echarts.use([BarChart, GridComponent, CanvasRenderer]);
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [CommonModule, NgxEchartsDirective],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css',
+  providers: [provideEchartsCore({ echarts })],
 })
 export class SummaryComponent {
   ngOnChanges(changes: SimpleChanges) {
@@ -16,6 +25,11 @@ export class SummaryComponent {
 
   @Input() transations: Transation[] = [];
 
+  // Pegar as os meses que são diferentes e escrever por extenso
+  dates: [] = [];
+  // Pegar as entradas no mes específico
+  // Pegar as saidas no mes específico
+  // separar dados por dicionario Ex Mes[TotalEntradas,TotalSaidas ]
   entry: number = 0;
   exit: number = 0;
   balance: number = 0;
@@ -47,6 +61,7 @@ export class SummaryComponent {
     if (this.balance < 0) this.balanceStatus = 'values red';
   }
 
+  // Options for cards
   changeOption(optionNumber: number) {
     if (optionNumber == 1) {
       this.options = 1;
@@ -59,4 +74,22 @@ export class SummaryComponent {
       this.options1 = '';
     }
   }
+
+  // Graphics
+  chartOption: EChartsCoreOption = {
+    xAxis: {
+      type: 'category',
+      data: this.transations.length,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Vendas',
+        data: [820, 932, 901],
+        type: 'bar',
+      },
+    ],
+  };
 }
